9096e000ef7605dc06bc6328120f9f5ceaaabc00
Mahima-Sahu <52346587+Mahima-Sahu@users.noreply.github.com>
2021-08-14 15:39:36 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-cd ${{github.workspace}}/student-service/student-service-app/target/student-service-app-1.0.0-SNAPSHOT.jar /myjar
-pause
+:: cd ${{github.workspace}}/student-service/student-service-app/target/student-service-app-1.0.0-SNAPSHOT.jar /myjar
+:: pause
+Call :UnZipFile "C:\Vectra_executables\" "student-service-app-1.0.0-SNAPSHOT.zip"
